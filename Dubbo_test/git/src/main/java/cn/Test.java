8dbeab638a0daa60b5eb34307091639c73cb28ea
chiyu <chiyu@.com>
2019-10-28 18:54:17 +0800

    把本地工程添加到git 2123

---
+++ Dubbo_test/git/src/main/java/cn/Test.java
@@ -0,0 +1,13 @@
+package cn;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git2 ");
+        System.out.println("123");
+    }
+}
